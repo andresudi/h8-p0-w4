6c4994c27d33cba9a22c5ca18f1cc2800b202687
Andre Sudi Yudatama <y.andresudi@gmail.com>
2018-05-22 23:04:53 +0700

    Add files via upload

---
+++ Exercise_14_MostFrequentNum.js
@@ -0,0 +1,47 @@
+
+// di sort dulu jadi angka terbesar ke kecil
+function sorting(arrNumber) {
+    // code di sini
+    arrNumber.sort(function (value1, value2) {
+        return value1 < value2
+    })
+    return arrNumber
+}
+
+// cari modus nya setelah udah di sort
+function getTotal(arrNumber) {
+    // code di sini
+    var counter = 0;
+    for (var i = 0; i < arrNumber.length; i++) {
+        if (arrNumber[0] === arrNumber[i]) {
+            counter++
+            //console.log(counter)
+        }
+    }
+
+    if (arrNumber.length > 0) {
+        var result = 'angka paling besar adalah ' + arrNumber[0] + ' dan jumlah kemunculan sebanyak ' + counter + ' kali'
+    } else if (arrNumber.length === 0) {
+        return "''"
+    }
+    return result
+    //console.log(mode)
+}
+
+function mostFrequentLargestNumbers(arrNumber) {
+    var listSort = sorting(arrNumber);
+    var countHighest = getTotal(listSort);
+    return countHighest;
+}
+
+console.log(mostFrequentLargestNumbers([2, 8, 4, 6, 8, 5, 8, 4]));
+//'angka paling besar adalah 8 dan jumlah kemunculan sebanyak 3 kali'
+
+console.log(mostFrequentLargestNumbers([122, 122, 130, 100, 135, 100, 135, 150]));
+//'angka paling besar adalah 150 dan jumlah kemunculan sebanyak 1 kali'
+
+console.log(mostFrequentLargestNumbers([1, 1, 1, 1]));
+//'angka paling besar adalah 1 dan jumlah kemunculan sebanyak 4 kali'
+
+console.log(mostFrequentLargestNumbers([]));
+//''
